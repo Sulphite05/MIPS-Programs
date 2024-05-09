beb75ddd38f4291e10d8ca6a5f89684828a0c5ee
Aqiba Abdul Qadir <aqibaabdulqadir@gmail.com>
2024-05-09 23:02:25 +0500

    Changes to be committed: 	modified:   prime_composite.asm

--- prime_composite.asm
+++ prime_composite.asm
@@ -2,6 +2,7 @@
     msg: .asciiz "Enter any integer: "
     prime: .asciiz "The number is prime."
     composite: .asciiz "The number is composite."
+    neither: .asciiz "The number is neither prime nor composite."
 
 .text
 .globl main
@@ -15,12 +16,27 @@ main:
     li $v0, 5           # take integer input
     syscall
     move $s0, $v0
+    
+    slti $t4, $s0, 2    # check if number is less than 2
+    beq $t4, $0, init   # if false, run calculations  
+    
+    li $v0, 4           # print neither if true
+    la $a0, neither
+    syscall
+    
+    j end
 
-    move $t0, $s0
-    li $t1, 1           # divisor
-    li $t2, 0           # counts factors
+    init:
+        li $t1, 2           # divisor/ loop iterator
+        li $t2, 2           # counts factors(all numbers greater than 1 have atleast 2 factors)
 
     loop:               # to do
+        beq $s0, $t1, prime_label       # if number and divsor are same, go print prime
+        div $s0, $t1                    # divide number
+        mfhi $t3                        # store remainder
+        beq $t3, $0, composite_label    # if it is a factor then we have more than 2 factors
+        addi $t1, $t1, 1                # increment iterator
+
         
     prime_label:
         li $v0, 4           # print prime
