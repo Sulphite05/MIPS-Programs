0bd064663422fb21bfd809cfe89c197b1f7e9f2e
Aqiba Abdul Qadir <aqibaabdulqadir@gmail.com>
2024-05-15 20:34:35 +0500

    Changes to be committed: 	new file:   power_till_n.asm

---
+++ power_till_n.asm
@@ -0,0 +1,76 @@
+.data
+    msg1: .asciiz "Enter the base: "
+    msg2: .asciiz "Enter the power: "
+    msg3: .asciiz "The answer is: "
+
+.text
+.globl main
+.ent main
+
+main:
+    li $v0, 4           # print msg1
+    la $a0, msg1
+    syscall
+
+    li $v0, 5           # take base input
+    syscall
+    move $a1, $v0
+
+    li $v0, 4           # print msg2
+    la $a0, msg2
+    syscall
+
+    li $v0, 5           # take power input
+    syscall
+    move $a2, $v0
+
+    addi $sp, $sp, -4
+    sw $ra, 0($sp)
+
+    jal calc_power
+
+    lw $ra, 0($sp)
+    addi $sp, $sp, 4
+
+    li $v0, 4           # print msg3
+    la $a0, msg3
+    syscall
+
+    li $v0, 1           
+    move $a0, $v1
+    syscall
+
+    jr $ra
+    
+
+
+
+calc_power:
+
+    addi $sp, $sp, -12                      # pushing saved reg
+    sw $s0, 0($sp)
+    sw $s1, 4($sp)
+    sw $s2, 8($sp)
+
+    move $s0, $a1       # base
+    move $s1, $a2       # power
+    addi $s2, $0, 1     # result
+
+    loop:
+        beq $s1, $0, END
+        mult $s2, $s0
+        mflo $s2
+        addi $s1, $s1, -1
+        j loop
+    
+    END:
+        move $v1, $s2
+
+        lw $s0, 0($sp)                      # poping saved reg
+        lw $s1, 4($sp)
+        lw $s2, 8($sp)
+        addi $sp, $sp, 12    
+
+        jr $ra 
+
+.end main
